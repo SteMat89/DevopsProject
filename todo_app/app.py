32d78aecbc9e977755e8a1ad3490f8969773215c
SteMat89 <stemat@GUPPY.zoo.lan>
2024-02-02 15:51:03 +0200

    Finish exercise 1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-
 from todo_app.flask_config import Config
+from flask import Flask, render_template, request, redirect, url_for
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +8,10 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=get_items())
+
+
+@app.route('/items', methods=['POST'])
+def post_item():
+    add_item(request.form['item-title'])
+    return redirect(url_for('index'))
